545dd2f2ea387d3369264da436f88a5a5d645942
annab857 <abunt855@gmail.com>
2026-02-06 21:49:45 +0100

    Create products listing page

---
+++ app/products/page.js
@@ -0,0 +1,14 @@
+// products listing page
+
+import React from 'react';
+
+const ProductsPage = () => {
+    return (
+        <div>
+            <h1>Products Listing</h1>
+            {/* Add your product listing logic here */}
+        </div>
+    );
+};
+
+export default ProductsPage;
