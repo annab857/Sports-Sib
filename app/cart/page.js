4abe84560e52891ff61d2b9dfb2edd35fc45003a
annab857 <abunt855@gmail.com>
2026-02-06 21:49:53 +0100

    Create shopping cart page

---
+++ app/cart/page.js
@@ -0,0 +1,14 @@
+// Shopping Cart Page
+
+import React from 'react';
+
+const CartPage = () => {
+    return (
+        <div>
+            <h1>Your Shopping Cart</h1>
+            {/* Add your cart items and logic here */}
+        </div>
+    );
+};
+
+export default CartPage;
