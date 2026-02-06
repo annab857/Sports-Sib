e5cfbe048c53909eda833e69e6b23811fbbefede
annab857 <abunt855@gmail.com>
2026-02-06 21:49:31 +0100

    Create home page for the Sports Sib application

---
+++ app/page.js
@@ -0,0 +1,10 @@
+// This is the home page for the Sports Sib application
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to Sports Sib</h1>
+            <p>Your go-to app for sports updates!</p>
+        </div>
+    );
+}
